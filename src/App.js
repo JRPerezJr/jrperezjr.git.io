3993207b9e0133588c1df8e66736446defde4e0c
Juan P <pront23@gmail.com>
2019-04-10 16:26:14 +0900

    cleaned app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection } from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import {
+  Layout,
+  Header,
+  Navigation,
+  Drawer,
+  Content,
+  Footer,
+  FooterSection
+} from "react-mdl";
+import Main from "./components/main";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout className="background-page">
-        <Header className='header-color' title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Portfolio</Link>} scroll>
+        <Layout className="background-page">
+          <Header
+            className="header-color"
+            title={
+              <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                Portfolio
+              </Link>
+            }
+            scroll
+          >
             <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Portfolio</Link>}>
+          </Header>
+          <Drawer
+            title={
+              <Link style={{ textDecoration: "none", color: "black" }} to="/">
+                Portfolio
+              </Link>
+            }
+          >
             <Navigation>
-            <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
-        </Drawer>
-        <Content>
-            <Main/>
-        </Content>
-        <Footer className="footer-color">
-            <FooterSection style={{height:".5rem"}} type="middle" logo="&copy; 2019 jperezdevinjp.tech ver. 1.0">
-            </FooterSection>
-        </Footer>
-    </Layout>
-</div>
+          </Drawer>
+          <Content>
+            <Main />
+          </Content>
+          <Footer className="footer-color">
+            <FooterSection
+              style={{ height: ".5rem" }}
+              type="middle"
+              logo="&copy; 2019 jperezdevinjp.tech ver. 1.0"
+            />
+          </Footer>
+        </Layout>
+      </div>
     );
   }
 }
